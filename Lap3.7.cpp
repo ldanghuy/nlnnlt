9db5e332fba4ce10218de7797823745ba389dccd
ldanghuy <151826466+ldanghuy@users.noreply.github.com>
2024-04-01 10:19:50 +0700

    Add files via upload

---
+++ Lap3.7.cpp
@@ -0,0 +1,37 @@
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int rows = 5; 
+    int cols = 5; 
+
+    int matrix[5][5] = {
+        {1, 2, 3, 4, 5},
+        {6, 7, 8, 9, 10},
+        {11, 12, 13, 14, 15},
+        {16, 17, 18, 19, 20},
+        {21, 22, 23, 24, 25}
+    };
+
+    for (int i = 0; i < rows; ++i) {
+        for (int j = 0; j < cols; ++j) {
+            cout << matrix[i][j] << " "; 
+        }
+        cout << endl; 
+    }
+
+    return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
