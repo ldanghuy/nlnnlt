efadb38ced08a606437b97177bcad220ae51e229
ldanghuy <151826466+ldanghuy@users.noreply.github.com>
2024-04-01 10:00:44 +0700

    Update Lap3.3.cpp

--- Lap3.3.cpp
+++ Lap3.3.cpp
@@ -1,6 +1,29 @@
-﻿Build started at 9:56 AM...
-1>------ Build started: Project: Lap3.3, Configuration: Debug x64 ------
-1>Lap3.3.cpp
-1>Lap3.3.vcxproj -> D:\New folder\Lap3.3\x64\Debug\Lap3.3.exe
-========== Build: 1 succeeded, 0 failed, 0 up-to-date, 0 skipped ==========
-========== Build completed at 9:56 AM and took 01.204 seconds ==========
+#include <iostream>
+
+using namespace std;
+
+
+double CalculateS(int n) {
+    double sum = 0.0;
+    int sign = 1; 
+
+    for (int i = 1; i <= n; ++i) {
+        sum += sign * (1.0 / i);
+        sign *= -1; 
+    }
+
+    return sum;
+}
+
+int main() {
+    int N;
+
+    cout << "Nhap vao so nguyen duong N: ";
+    cin >> N;
+
+    double result = CalculateS(N);
+
+    cout << "Tong S(" << N << ") = " << result << endl;
+
+    return 0;
+}
