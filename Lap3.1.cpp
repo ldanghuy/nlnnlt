8a87eb3c3335f2e1f5a65e160c0c88b42c8492e4
ldanghuy <151826466+ldanghuy@users.noreply.github.com>
2024-04-01 09:45:15 +0700

    Update Lap3.1.cpp

--- Lap3.1.cpp
+++ Lap3.1.cpp
@@ -1,14 +1,36 @@
-﻿Build started at 9:35 AM...
-1>------ Build started: Project: Lap3.1, Configuration: Debug x64 ------
-1>Lap3.1.cpp
-1>D:\New folder\Lap3.1\Lap3.1\Lap3.1.cpp(23,13): warning C4566: character represented by universal-character-name '\u1EAD' cannot be represented in the current code page (1252)
-1>D:\New folder\Lap3.1\Lap3.1\Lap3.1.cpp(23,13): warning C4566: character represented by universal-character-name '\u01A1' cannot be represented in the current code page (1252)
-1>D:\New folder\Lap3.1\Lap3.1\Lap3.1.cpp(23,13): warning C4566: character represented by universal-character-name '\u1ED1' cannot be represented in the current code page (1252)
-1>D:\New folder\Lap3.1\Lap3.1\Lap3.1.cpp(26,13): warning C4566: character represented by universal-character-name '\u1EAD' cannot be represented in the current code page (1252)
-1>D:\New folder\Lap3.1\Lap3.1\Lap3.1.cpp(26,13): warning C4566: character represented by universal-character-name '\u1ED1' cannot be represented in the current code page (1252)
-1>D:\New folder\Lap3.1\Lap3.1\Lap3.1.cpp(26,13): warning C4566: character represented by universal-character-name '\u0169' cannot be represented in the current code page (1252)
-1>D:\New folder\Lap3.1\Lap3.1\Lap3.1.cpp(30,25): warning C4244: 'argument': conversion from 'float' to 'int', possible loss of data
-1>Lap3.1.vcxproj -> D:\New folder\Lap3.1\x64\Debug\Lap3.1.exe
-1>Done building project "Lap3.1.vcxproj".
-========== Build: 1 succeeded, 0 failed, 0 up-to-date, 0 skipped ==========
-========== Build completed at 9:35 AM and took 00.825 seconds ==========
+// Lap3.1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <cmath> // Thư viện cho hàm pow()
+
+using namespace std;
+
+// Hàm tính lũy thừa a^n
+double Power_n(int x, int n) {
+    double result = 1;
+    // Sử dụng vòng lặp for với điều kiện n--
+    for (int i = 0; i < n; ++i) {
+        result *= x;
+    }
+    return result;
+}
+
+int main() {
+    float a;
+    int n;
+
+    cout << "Nhập vào cơ số a: ";
+    cin >> a;
+
+    cout << "Nhập vào số mũ n: ";
+    cin >> n;
+
+    // Gọi hàm Power_n() để tính a^n
+    double an = Power_n(a, n);
+
+    cout << fixed; // Hiển thị số thập phân cố định
+    cout << a << "^" << n << " = " << an << endl;
+
+    return 0;
+}
