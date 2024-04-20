3c62742a5f3f6399f2ecc2cad264fbe522062ed0
ldanghuy <151826466+ldanghuy@users.noreply.github.com>
2024-04-20 14:14:02 +0700

    Add files via upload

---
+++ lab6_bai4.cpp
@@ -0,0 +1,29 @@
+﻿#include<iostream>
+#include<string>
+using namespace std;
+
+string xoaKhoangTrangDauCuoi(string str) {
+    
+    while (!str.empty() && isspace(str.front())) {
+        str.erase(str.begin());
+    }
+
+    
+    while (!str.empty() && isspace(str.back())) {
+        str.pop_back();
+    }
+
+    return str;
+}
+
+int main() {
+    string chuoi;
+    cout << "Nhập chuỗi: ";
+    getline(cin, chuoi);
+
+    chuoi = xoaKhoangTrangDauCuoi(chuoi);
+
+    cout << "Chuỗi sau khi xóa khoảng trắng ở đầu và cuối: '" << chuoi << "'" << endl;
+
+    return 0;
+}
