ebcaa4623a7013f2edb5a124d0d74ecd1d11fbd0
ldanghuy <151826466+ldanghuy@users.noreply.github.com>
2024-04-20 14:13:08 +0700

    Add files via upload

---
+++ lab6_bai1.cpp
@@ -0,0 +1,19 @@
+﻿#include<iostream>
+#include<string>
+using namespace std;
+
+int main() {
+    string chuoiBanDau ;
+    string chuoiChiDinh ;
+    cout << " Nhập chuỗi ban đầu: ";
+    cin >> chuoiBanDau;
+    cout << " Nhập chuỗi sau đầu: ";
+    cin >> chuoiChiDinh;
+    for (int i = 0; i < chuoiChiDinh.length(); i++) {
+        chuoiBanDau += chuoiChiDinh[i];
+    }
+
+    cout << "Chuỗi sau khi nối: " << chuoiBanDau << endl;
+
+    return 0;
+}
